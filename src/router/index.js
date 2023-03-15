f988bd5cc60fa157bd1961f5c167f957118eb42d
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-03-15 17:29:01 +0800

    resolve routing scroll behaviour prob

--- src/router/index.js
+++ src/router/index.js
@@ -71,7 +71,10 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
+    scrollBehavior() {
+        document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
+    }
 })
 
 export default router
