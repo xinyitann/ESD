a82665b9560c38c9326c433a86fa730d2d1b22e0
Xinyi Tan <xinyi.tan.2021@scis.smu.edu.sg>
2023-03-15 15:22:43 +0800

    general layout for property details

--- src/views/properties.vue
+++ src/views/properties.vue
@@ -22,7 +22,7 @@
         <div class="row justify-content-center" style="border-bottom-color: var(--bs-pink);box-shadow: 0px 0px 0px;text-align: center;">
             <div class="col-4 col-md-3 col-xl-6"><input type="search" placeholder="Search">
                 <div class="btn-group"><button class="btn btn-light" type="button">Property</button><button class="btn btn-light dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false" type="button"></button>
-                    <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
+                    <div class="dropdown-menu"><a class="dropdown-item" href="#">Condo</a><a class="dropdown-item" href="#">HDB</a><a class="dropdown-item" href="#">Landed</a></div>
                 </div>
                 <div class="btn-group"><button class="btn btn-light" type="button">Price</button><button class="btn btn-light dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false" type="button"></button>
                     <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
@@ -38,12 +38,12 @@
         <div class="row gy-4 row-cols-1 row-cols-xl-3 text-start justify-content-center">
             <div class="col-xl-9">
                 <div class="card">
-                    <div class="card-body shadow p-4">
+                    <div class="card-body shadow " style="padding-top: 0px;padding-left: 0px;padding-right: 0px;">
                         <div class="carousel slide" data-bs-ride="false" id="carousel-1">
                             <div class="carousel-inner">
-                                <div class="carousel-item active"><img class="w-100 d-block" src="../assets/room1.jpg" alt="Slide Image" ></div>
-                                <div class="carousel-item"><img class="w-100 d-block" src="../assets/room1.jpg" alt="Slide Image"></div>
-                                <div class="carousel-item"><img class="w-100 d-block" src="../assets/room1.jpg" alt="Slide Image"></div>
+                                <div class="carousel-item active"><img class="w-100 d-block fit-cover" src="../assets/room2.jpg" alt="Slide Image" ></div>
+                                <div class="carousel-item"><img class="w-100 d-block" src="../assets/room2.jpg" alt="Slide Image"></div>
+                                <div class="carousel-item"><img class="w-100 d-block" src="../assets/room2.jpg" alt="Slide Image"></div>
                             </div>
                             <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
                             <ol class="carousel-indicators">
@@ -53,12 +53,13 @@
                             </ol>
                         </div>
                         <br>
-                        <h5 class="card-text">Blk 345<br>Halloween Avenue 6<br>Singapore 672044</h5>
-                        <h5 class="card-text">1216 sqft</h5>
-                        <h5 class="card-text">$600,000</h5>
-                        <h5 class="card-text text-end">Bidding in progress</h5>
-                        <button class="btn btn-light float-end" type="button">View Details</button>
-                        
+                        <div class="card-body p-4">
+                            <h5 class="card-text">Blk 345<br>Halloween Avenue 6<br>Singapore 672044</h5>
+                            <h5 class="card-text">1216 sqft</h5>
+                            <h5 class="card-text">$600,000</h5>
+                            <h5 class="card-text text-end">Bidding in progress</h5>
+                            <a class="btn btn-light float-end" href="propertyDetails" type="button" style="background-color: #447098; color: white;" >View Details</a>
+                        </div>
                         <!-- <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
                             <div>
                                 <p class="fw-bold mb-0">John Smith</p>
--- src/views/propertyDetails.vue
+++ src/views/propertyDetails.vue
@@ -1,6 +1,94 @@
 <template>
     <div>
 
+        <!--Carousel room images-->
+    <div class="carousel slide" data-bs-ride="false" id="carousel-1">
+        <div class="carousel-inner">
+            <div class="carousel-item active"><img class="w-100 d-block" src="../assets/room2.jpg" alt="Slide Image" width="1393" height="525"></div>
+            <div class="carousel-item"><img class="w-100 d-block" src="../assets/room2.jpg" alt="Slide Image" width="1393" height="525"></div>
+            <div class="carousel-item"><img class="w-100 d-block" src="../assets/room2.jpg" alt="Slide Image" width="1393" height="525"></div>
+        </div>
+        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
+        </ol>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-xxl-8">
+                        <div class="card border-light">
+                            <div class="card-body" style="background: var(--bs-white);border-style: none;">
+                                <h4 class="text-center card-title">$600,000</h4>
+                            </div>
+                        </div>
+                        <p>Squarefeet : 1216 sqft</p>
+                        <p>Room : 3 bedrooms, 2 toilets</p>
+                        <p>Address :&nbsp;Blk 345<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Halloween Avenue 6<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Singapore 672044</p>
+                        <p>Build Year : 1995</p>
+                    </div>
+                    <div class="col"></div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card border-light"></div>
+                    </div>
+                </div>
+            </div>
+
+            <!--Right half-->
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col"></div>
+                    <div class="col-xxl-8">
+                        <div class="card border-light">
+
+                            <!--Agent information-->
+                            <div class="card-body" style="background: var(--bs-gray-100);">
+                                <h4 class="text-center card-title">Jonah</h4>
+                                <div class="row">
+                                    <div class="col d-xxl-flex justify-content-xxl-center align-items-xxl-center" style="text-align: center;">
+                                        <p class="fs-5">Contact Information&nbsp;</p>
+                                    </div>
+                                    <div class="col">
+                                        <p style="text-align: center;">+65 12345678&nbsp;</p>
+                                        <p style="text-align: center;">hello@gmail.com&nbsp;</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col"></div>
+                                    <div class="col-xxl-6 d-xxl-flex justify-content-xxl-center align-items-xxl-center"><a class="btn btn-danger float-start" type="button" href="booking">Book Appointment</a></div>
+                                    <div class="col"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col"></div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card border-light">
+
+                            <!--Bidding information-->
+                            <div class="card-body">
+                                <h4 class="text-center card-title">Bidding Details&nbsp;</h4>
+                                <p class="d-xxl-flex justify-content-xxl-center card-text">5 days 3 hrs / Sunday 10 pm / 7 bids / $ 900,000</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+
     </div>
 </template>
 
