154698947addc7c6c00eef316392447abe2bcc14
faithangg <89286947+faithangg@users.noreply.github.com>
2023-03-17 20:29:16 +0800

    add_listing.py -complex microservice
    not completed, added to test error microservice.
    calls to property microservice made here

---
+++ microservices/add_listing.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os, sys
+
+import requests
+from invokes import invoke_http
+
+import amqp_setup
+import pika
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+property_URL = "http://localhost:5001/property"
+
+@app.route("/add_listing", methods=['POST'])
+def add_listing():
+    # Simple check of input format and data of the request are JSON
+    if request.is_json:
+        try:
+            property = request.get_json()
+            print("\nReceived a property listing in JSON:", property)
+
+            # do the actual work
+            # 1. Send property info {propety_listing}
+            result = processAddListing(property)
+            print('\n------------------------')
+            print('\nresult: ', result)
+            return jsonify(result), result["code"]
+
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "place_property.py internal error: " + ex_str
+            }), 500
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+
+def processAddListing(property):
+    # 2. Send the property info {}
+    # Invoke the property microservice
+    print('\n-----Invoking property microservice-----')
+    property_result = invoke_http(property_URL, method='POST', json=property)
+    print('property_result:', property_result)
+  
+
+    # Check the property result; if a failure, send it to the error microservice. => listing or bidding information is incorrect (from google docs)
+    code = property_result["code"]
+    message = json.dumps(property_result)
+
+    if code not in range(200, 300):
+        # Inform the error microservice
+        print('\n\n-----Publishing the (property error) message with routing_key=property.error-----')
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="property.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+     
+        print("\nproperty status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), property_result)
+
+        # 7. Return error
+        return {
+            "code": 500,
+            "data": {"property_result": property_result},
+            "message": "property creation failure sent for error handling."
+        }
+   
+    print("\nproperty published to RabbitMQ Exchange.\n")
+    # - reply from the invocation is not used;
+    # continue even if this invocation fails
+
+    # 7. Return created property
+    return {
+        "code": 201,
+        "data": {
+            "property_result": property_result,
+        }
+    }
+
+
+# Execute this program if it is run as a main script (not by 'import')
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) + " for adding a property...")
+    app.run(host="0.0.0.0", port=5100, debug=True)
