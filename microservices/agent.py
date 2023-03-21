1b99dfc851b17597cd0761246a6c5229de5cef4b
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-03-22 01:14:18 +0800

    completed agent.py

--- microservices/agent.py
+++ microservices/agent.py
@@ -18,6 +18,102 @@ class Agent(db.Model):
     def json(self):
         return {"agent_id": self.agent_id, "name": self.name, "phone": self.phone, "email": self.email}
 
+# GET ONE AGENT
+@app.route("/agent/<string:agent_id>")
+def find_agent(agent_id):
+    agent = Agent.query.filter_by(agent_id=agent_id).first()
+    if agent:
+        return jsonify(
+            {
+                "code": 200,
+                "data": agent.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "agent not found."
+        }
+    ), 404
+
+# ADDING A AGENT
+@app.route("/agent", methods=['POST'])
+def create_agent():
+    data = request.get_json()
+    agent = Agent(**data)
+
+    try:
+        db.session.add(agent)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred creating the agent."
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": agent.json()
+        }
+    ), 201
+
+# UPDATE AGENT DETAILS
+@app.route("/agent/<string:agent_id>", methods=['PUT'])
+def update_agent(agent_id):
+    agent = Agent.query.filter_by(agent_id=agent_id).first()
+    if agent:
+        data = request.get_json()
+        if data['name']:
+            agent.name = data['name']
+        if data['phone']:
+            agent.phone = data['phone']
+        if data['email']:
+            agent.email = data['email'] 
+        db.session.commit()
+        return jsonify(
+            {
+                "code": 200,
+                "data": agent.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "agent_id": agent_id
+            },
+            "message": "agent not found."
+        }
+    ), 404
+
+#DELETE AGENT
+@app.route("/agent/<string:agent_id>", methods=['DELETE'])
+def delete_agent(agent_id):
+    agent = Agent.query.filter_by(agent_id=agent_id).first()
+    if agent:
+        db.session.delete(agent)
+        db.session.commit()
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "agent_id": agent_id
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "agent_id": agent_id
+            },
+            "message": "agent not found."
+        }
+    ), 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
