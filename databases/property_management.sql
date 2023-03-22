1f7266197acb19efb502e90235cf7b6fde3e6ca5
Balveen Kaur <balveenkaur01@gmail.com>
2023-03-22 18:02:13 +0800

    database changes

--- databases/property_management.sql
+++ databases/property_management.sql
@@ -13,6 +13,13 @@ USE `property_management`;
 --
 -- Table structure for table `agent`
 --
+DROP TABLE IF EXISTS `property_images`;
+DROP TABLE IF EXISTS `booking`;
+DROP TABLE IF EXISTS `bids`;
+DROP TABLE IF EXISTS `property`;
+DROP TABLE IF EXISTS `auctions`;
+
+
 
 DROP TABLE IF EXISTS `agent`;
 CREATE TABLE IF NOT EXISTS `agent` (
@@ -63,12 +70,48 @@ CREATE TABLE property_images(
     PRIMARY KEY(`propertyID_image`)
 );
 
+--
+-- Table structure for table `auctions`
 
+DROP TABLE IF EXISTS `auctions`;
+CREATE TABLE IF NOT EXISTS `auctions` (
+  `auction_id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` TIMESTAMP NOT NULL,
+  `end_time` TIMESTAMP NOT NULL,
+  `starting_price` FLOAT(53) NOT NULL,
+  `option_fee` FLOAT(53) NOT NULL,
+  `highest_bid` FLOAT(2),
+  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+  `updated_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`auction_id`)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `auctions` (`auction_id`, `start_time`, `end_time`, `starting_price`,`option_fee`, `highest_bid`, `created_at`, `updated_at`)VALUES 
+('1', '2023-03-23 12:00:00', '2023-03-27 12:00:00', '13150000','1000', '20000', '2023-03-20 12:00:00','2023-03-20 12:00:00');
+
+
+-- Table structure for table `bids`
+DROP TABLE IF EXISTS `bids`;
+CREATE TABLE IF NOT EXISTS `bids` (
+  `bid_id`int(11) NOT NULL AUTO_INCREMENT,
+  `auction_id` INT NOT NULL,
+  `customer_id` INT NOT NULL,
+  `bid_amount` FLOAT(53) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+  `updated_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`bid_id`),
+  constraint bids_fk1 foreign key (auction_id) references auctions(auction_id),
+  constraint bids_fk2 foreign key (customer_id) references customer(customer_id)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `bids` (`bid_id`, `auction_id`, `customer_id`, `bid_amount`, `created_at`, `updated_at`)VALUES 
+('1', '1', '1', '22250000','2023-03-22 12:00:00','2023-03-22 12:00:00');
 
 -- Table structure for table `property`
 DROP TABLE IF EXISTS property;
 CREATE TABLE IF NOT EXISTS property (
   `property_id` int NOT NULL AUTO_INCREMENT,
+  `auction_id` int(11) NOT NULL,
   `agent_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
@@ -83,14 +126,15 @@ CREATE TABLE IF NOT EXISTS property (
   `image` varchar(255) NOT NULL, -- store the file path instead of varbinary data
   PRIMARY KEY (`property_id`),
   constraint property_fk1 foreign key (agent_id) references agent(agent_id),
-  constraint property_fk2 foreign key (customer_id) references customer(customer_id)
+  constraint property_fk2 foreign key (customer_id) references customer(customer_id),
+  constraint property_fk3 foreign key (auction_id) references auctions(auction_id)
 );
 
 
 --
 -- Dumping data for table `property`
-INSERT INTO `property` (`property_id`, `agent_id`,`customer_id`, `name`, `address`, `postalcode`,`property_type`, `square_feet`, `room`, `facing`,`build_year`, `estimated_cost`,`image`) VALUES
-('1','1','1', 'Waterfront condominium', 'waterfront street 17', '123456', 'Condominium','5929',  '4', 'north','2011', '13150000','');
+INSERT INTO `property` (`property_id`, `auction_id`, `agent_id`,`customer_id`, `name`, `address`, `postalcode`,`property_type`, `square_feet`, `room`, `facing`,`build_year`, `estimated_cost`,`image`) VALUES
+('1','1','1','1', 'Waterfront condominium', 'waterfront street 17', '123456', 'Condominium','5929',  '4', 'north','2011', '13150000','');
 
 
 DROP TABLE IF EXISTS property_images;
@@ -138,43 +182,6 @@ CREATE TABLE IF NOT EXISTS `error` (
 INSERT INTO `error` (`error_id`,`error_message`)
 VALUES ('1', 'Error: Booking is no longer available');
 
---
--- Table structure for table `auctions`
-
-DROP TABLE IF EXISTS `auctions`;
-CREATE TABLE IF NOT EXISTS `auctions` (
-  `auction_id` int(11) NOT NULL AUTO_INCREMENT,
-  `property_id` int(11) NOT NULL,
-  `start_time` TIMESTAMP NOT NULL,
-  `end_time` TIMESTAMP NOT NULL,
-  `starting_price` FLOAT(53) NOT NULL,
-  `option_fee` FLOAT(53) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `updated_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  PRIMARY KEY (`auction_id`),
-  constraint auction_fk1 foreign key (property_id) references property(property_id)
-)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-INSERT INTO `auctions` (`auction_id`, `property_id`, `start_time`, `end_time`, `starting_price`,`option_fee`, `created_at`, `updated_at`)VALUES 
-('1', '1', '2023-03-23 12:00:00', '2023-03-27 12:00:00', '13150000','1000','2023-03-20 12:00:00','2023-03-20 12:00:00');
-
-
--- Table structure for table `bids`
-DROP TABLE IF EXISTS `bids`;
-CREATE TABLE IF NOT EXISTS `bids` (
-  `bid_id`int(11) NOT NULL AUTO_INCREMENT,
-  `auction_id` INT NOT NULL,
-  `customer_id` INT NOT NULL,
-  `bid_amount` FLOAT(53) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `updated_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  PRIMARY KEY (`bid_id`),
-  constraint bids_fk1 foreign key (auction_id) references auctions(auction_id),
-  constraint bids_fk2 foreign key (customer_id) references customer(customer_id)
-)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-INSERT INTO `bids` (`bid_id`, `auction_id`, `customer_id`, `bid_amount`, `created_at`, `updated_at`)VALUES 
-('1', '1', '1', '22250000','2023-03-22 12:00:00','2023-03-22 12:00:00');
 
 DROP TABLE IF EXISTS `extra`;
 CREATE TABLE IF NOT EXISTS `extra` (
