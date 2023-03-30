fdf7d2c911faefd39010bfe19e54754cf54c71a4
Balveen Kaur <balveenkaur01@gmail.com>
2023-03-30 19:52:30 +0800

    changes to accept booking

--- microservices/accept_booking.py
+++ microservices/accept_booking.py
@@ -4,10 +4,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import os, sys
+from os import environ
 
 import requests
 from invokes import invoke_http
-from os import environ
 
 import amqp_setup
 import pika
@@ -18,7 +18,8 @@ CORS(app)
 
 booking_URL = environ.get('booking_URL') or "http://localhost:5005/booking"
 customer_URL = environ.get('customer_URL') or "http://localhost:5700/customer"
-
+property_URL = environ.get('property_URl') or "http://localhost:5001/property"
+agent_URL = environ.get('agent_URl') or "http://localhost:5003/agent"
 
 def validate_booking_input(booking_details):
     # will have to get agent_id from agent profile somehow
@@ -62,37 +63,58 @@ def accept_booking():
 
             # get the customer id
             customer_id = booking_result["data"]['booking_result']["data"]["customer_id"]
+
+            # get the agent id
+            agent_id = booking_result["data"]['booking_result']["data"]["agent_id"]
             
             # check what is the status of the ticket
             status = booking_result["data"]['booking_result']["data"]["status"]
+
+            # get the customer email and name 
+            customer_info_result = get_customer_info(customer_id)
+            print("customer_info_result ", customer_info_result)
+
+            customer_email = customer_info_result["data"]['customer_result']["data"]["email"]
+            customer_name =  customer_info_result["data"]['customer_result']["data"]["name"]
+
+            # get the agent name 
+            agent_info_result = get_agent_info(agent_id)
+            print("agent_info_result ", agent_info_result)
+            agent_name = agent_info_result["data"]['agent_result']["data"]["name"]
+            print("agent_name ", agent_name)
+            
+            # get the name of the property
+            property_info_result = get_property_info(customer_id)
+            print("property_info_result ", property_info_result)
+            property_name = property_info_result["data"]["property_result"]["data"]["name"]
+
+            start_time = booking_result["data"]['booking_result']["data"]["datetimestart"]
+            start_time = datetime.datetime.strptime(start_time,'%a, %d %b %Y %H:%M:%S GMT')
 
             # if the status is accepted
             if booking_result['code'] == 201 and status == "accepted":
                 
                 # get the start and end data of the booking
-                start_time = booking_result["data"]['booking_result']["data"]["datetimestart"]
                 end_time = booking_result["data"]['booking_result']["data"]["datetimeend"]
-                print(start_time,'starttime1')
-                print(end_time,'starttime2')
-                start_time = datetime.datetime.strptime(start_time,'%a, %d %b %Y %H:%M:%S GMT')
-                end_time =datetime.datetime.strptime(end_time,'%a, %d %b %Y %H:%M:%S GMT')
-                print(start_time)
-                print(end_time)
+                # end_time =start_time + timedelta(hours=1)
+                end_time = datetime.datetime.strptime(end_time,'%a, %d %b %Y %H:%M:%S GMT')
 
                 google_booking = {
                     "customer_id" : customer_id,
                     "start": start_time.strftime("%Y-%m-%dT%H:%M:%S"), 
-                    "end": end_time.strftime("%Y-%m-%dT%H:%M:%S")
+                    "end": end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+                    "property_name": property_name, 
+                    "customer_name": customer_name
                 } 
 
-                # add the booking into google calendar
+                # add the booking into google calendar (with the customer and property name)
                 google_result = add_google_calendar(google_booking)
                 print('\n------------------------')
                 print('\ngoogle_result: ', google_result)
                 
             
             # send the customer notification
-            send_customer_notification(customer_id, status)
+            send_customer_notification(customer_name, customer_email, status, property_name, start_time.strftime("%Y-%m-%dT%H:%M:%S"), agent_name)
             print('\nbooking_result: ', booking_result) 
             return jsonify(booking_result), booking_result["code"]
 
@@ -188,16 +210,127 @@ def add_google_calendar(gooogle_booking):
         }
     }
 
+# get the customer info
+def get_customer_info(customer_id):
+    print('\n-----Invoking customer microservice -----')
+    customer_id = str(customer_id)
+    get_customer_URL = customer_URL + "/" + customer_id
+    customer_result = invoke_http(get_customer_URL, method='GET', json=None)
 
-def send_customer_notification(customer_id, status):
-        customer_id = str(customer_id)
-        get_customer_URL = customer_URL + "/" + customer_id
-        # get the customer email
-        customer_result = invoke_http(get_customer_URL, method='GET', json=None)
+    code = customer_result["code"]
+    message = json.dumps(customer_result)
+
+    if code not in range(200, 300):
+        # Inform the error microservice
+        print('\n\n-----Publishing the (booking error) message with routing_key=property.error-----')
+
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="booking.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        print("booking status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), customer_result)
+
+        print("booking published to RabbitMQ Exchange.\n")\
+
+        return {
+            "code": 500,
+            "data": {"customer_result": customer_result},
+            "message": "unable to find customer."
+        }
+
+    # if reached here, no error & booking in google calendar is successfully created
+    return {
+        "code": 201,
+        "data": {
+            "customer_result": customer_result,
+        }
+    }
+
+
+# get the agent info
+def get_agent_info(agent_id):
+    print('\n-----Invoking agent microservice -----')
+    agent_id = str(agent_id)
+    get_agent_URL = agent_URL + "/" + agent_id
+    agent_result = invoke_http(get_agent_URL, method='GET', json=None)
+
+    code = agent_result["code"]
+    message = json.dumps(agent_result)
+
+    if code not in range(200, 300):
+        # Inform the error microservice
+        print('\n\n-----Publishing the (booking error) message with routing_key=property.error-----')
+
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="booking.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        print("booking status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), agent_result)
+
+        print("booking published to RabbitMQ Exchange.\n")\
+
+        return {
+            "code": 500,
+            "data": {"agent_result": agent_result},
+            "message": "unable to find agent."
+        }
+
+    # if reached here, no error & booking in google calendar is successfully created
+    return {
+        "code": 201,
+        "data": {
+            "agent_result": agent_result,
+        }
+    }
+
+# get the property info using customerid
+def get_property_info(customer_id):
+    print('\n-----Invoking property microservice -----')
+    customer_id = str(customer_id)
+    get_property_URL = property_URL + "/details/customer/" + customer_id
+    property_result = invoke_http(get_property_URL, method='GET', json=None)
+
+    code = property_result["code"]
+    message = json.dumps(property_result)
+
+    if code not in range(200, 300):
+        # Inform the error microservice
+        print('\n\n-----Publishing the (booking error) message with routing_key=property.error-----')
+
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="booking.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        print("booking status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), property_result)
+
+        print("booking published to RabbitMQ Exchange.\n")\
+
+        return {
+            "code": 500,
+            "data": {"property_result": property_result},
+            "message": "unable to find customer."
+        }
+
+    # if reached here, no error & booking in google calendar is successfully created
+    return {
+        "code": 201,
+        "data": {
+            "property_result": property_result,
+        }
+    }
+
+def send_customer_notification(name, email, status, property_name, start_time, agent_name):
+        # customer_id = str(customer_id)
+        # get_customer_URL = customer_URL + "/" + customer_id
+        # # get the customer email
+        # customer_result = invoke_http(get_customer_URL, method='GET', json=None)
 
         name_email = {
-            'name' : customer_result['data']['name'],
-            'email' : customer_result['data']['email']
+            'name' : name,
+            'email' : email,
+            'property_name': property_name, 
+            'start_time': start_time,
+            "agent_name": agent_name
         }
 
         # if the status is accepted send the customer the accepted notification
@@ -211,8 +344,6 @@ def send_customer_notification(customer_id, status):
             print('\n\n-----Calling Notification with routing_key=booking_rejected.notification-----')
             amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="booking_rejected.notification", 
             body=json.dumps(name_email), properties=pika.BasicProperties(delivery_mode = 2)) 
-
-
 
 # Execute this program if it is run as a main script (not by 'import')
 if __name__ == "__main__":
