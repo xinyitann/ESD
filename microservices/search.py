730c6ff45f91b86427e7076ae46ca9269235653c
Xinyi Tan <xinyi.tan.2021@scis.smu.edu.sg>
2023-03-30 12:40:05 +0800

    search.py
    removing the original search.py and adding new_search.py

--- microservices/search.py
+++ microservices/search.py
@@ -1,8 +1,172 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os, sys
+
+import requests
+from invokes import invoke_http
+
+import amqp_setup
+import pika
+import json
+
 import requests
 import json
 from dotenv import load_dotenv
 load_dotenv()
-import os
+
+app = Flask(__name__)
+CORS(app)
+
+property_URL = "http://localhost:5001/property"
+agent_URL="http://localhost:5003/agent"
+
+# validate seach input
+def validate_search_input(search_details):
+    # will have to get agent_id from agent profile somehow
+    if any( c.isalnum() for c in search_details):
+        return True
+    
+    else:
+        return False
+
+@app.route("/search_list", methods=['GET'])
+def process_search():
+    # Simple check of input format and data of the request are JSON
+    if request.is_json:
+        try:
+            search_details = request.get_json()
+            print("\nReceived a search in JSON:", search_details)
+
+            # Validate accept search input
+            if not validate_search_input(search_details):
+                # Inform the error microservice
+                error_message = {
+                    "code": 400,
+                    "message": "Invalid accept search input: missing or invalid required fields."
+                }
+                print('\n\n-----Publishing the (search input error) message with routing_key=search.error-----')
+                amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="search.error", 
+                        body=json.dumps(error_message), properties=pika.BasicProperties(delivery_mode = 2)) 
+                print("\nInvalid search input published to the RabbitMQ Exchange.\n")
+
+                return jsonify({
+                    "code": 400,
+                    "message": "Invalid accept search input: missing or invalid required fields."
+                }), 400
+
+            #check if postal code or neighbourhood
+            if len(search_details['postalcode'])==6:
+                #if postal code
+              print('here is property by postal code')
+              print(search_details)
+              return get_properties_from_postalcodes(search_details['postalcode'])
+              
+
+            else:
+                #if neighbourhood
+                print('here is property by neighbourhood')
+                print(search_details)
+                return get_properties_by_neighbourhood(search_details)
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "search.py internal error: " + ex_str
+            }), 500
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+def get_properties_by_neighbourhood(search_details):
+    print('\n-----Invoking property microservice for neighbourhood-----')
+    # neighbourhood = "Holland"
+    updated_property_URL=property_URL+'/neighbourhood/'+ search_details["neighbourhood"]
+    property_result = invoke_http(updated_property_URL,method='GET',json=None)
+    print('property_result from property microservice:', property_result)
+
+    code = property_result["code"]
+    message = json.dumps(property_result)
+
+    if code not in range(200, 300):
+        # Inform the error microservice
+        print('\n\n-----Publishing the (search error) message with routing_key=search.error-----')
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="search.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        print("\nsearch status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), property_result)
+
+        print("\nsearch published to RabbitMQ Exchange.\n")\
+
+
+        return {
+            "code": 500,
+            "data": {"property_result": property_result},
+            "message": "search creation failure sent for error handling."
+        }
+
+    return {
+    "code": 201,
+    "data": {
+        "property_result": property_result,
+    }
+}
+
+#Get properties from list of postal codes 
+def get_properties_from_postalcodes(postalcode):
+
+    postalcode_list=list_from_postal_code_input(postalcode)
+    postalcode_str=str(postalcode_list)
+    print(postalcode_str)
+    # print(postalcode_list) #['530321', '530323', '530325', '530320', '530324', '530322', '530322', '530326', '530333', '530332']
+
+    # if  postalcode_list:
+    #     for postalcode in postalcode_list:
+    #       print(postalcode)
+    print('\n-----Invoking property microservice for postalcode-----')
+    updated_property_URL=property_URL+'/postal_code/'+ postalcode_str
+    property_result = invoke_http(updated_property_URL,method='GET',json=None)
+    print('property_result from property microservice:', property_result)
+
+    code = property_result["code"]
+    message = json.dumps(property_result)
+    print(message)
+    if code not in range(200, 300):
+        # Inform the error microservice
+        print('\n\n-----Publishing the (search error) message with routing_key=search.error-----')
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="search.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        print("\nsearch status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), property_result)
+
+        print("\nsearch published to RabbitMQ Exchange.\n")\
+
+
+        return {
+            "code": 500,
+            "data": {"property_result": property_result},
+            "message": "search creation failure sent for error handling."
+        }
+
+    return {
+    "code": 201,
+    "data": {
+        "property_result": property_result,
+    }
+}
+    # else:
+    #     print("line 166")
+
 
 # to convert postal code given by user to coordinates
 def convert_postal_code_to_coordinates(postalCode):
@@ -68,5 +232,10 @@ def list_from_postal_code_input(searchInput):
         postalCodes.append(building["POSTALCODE"])
     return postalCodes
 
-searchInput = '530324'
-print(list_from_postal_code_input(searchInput))
+# searchInput = '530324'
+# print(list_from_postal_code_input(searchInput))
+
+# Execute this program if it is run as a main script (not by 'import')
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) + " for searching...")
+    app.run(host="0.0.0.0", port=5106, debug=True)
