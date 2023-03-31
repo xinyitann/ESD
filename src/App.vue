610c47023bc84a1ed12ae9e04a8d1217ddb2d5da
Abhisit7345 <111484215+Abhisit7345@users.noreply.github.com>
2023-03-31 23:07:56 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <NavBar></NavBar>
-  <router-view></router-view>
+  <router-view @passdata="GetData($event)" @agent_id="GetAgent($event)" :customer_id_prop='customer_id' :agent_id_prop='agent_id' :user_type="user_type"></router-view>
   <FooterComponent></FooterComponent>
+  <button @click="check_id()"></button> 
 </template>
 
 <script>
@@ -13,6 +14,27 @@ export default {
   components: {
     NavBar,
     FooterComponent,
+  },
+  data(){
+    return {
+      customer_id: 24,   //random initial variable for testing will get changed after logging in
+      agent_id: 67,   //random initial variable for testing will get changed after logging in
+      user_type: 'agent',   //random initial variable for testing will get changed after logging in
+    }
+  },
+  methods: {
+    GetData(data){
+      this.customer_id = data
+    },
+    GetAgent(data){
+      this.agent_id = data
+    },
+    check_id(){
+      console.log(this.agent_id)
+    }
+  },
+  created(){
+    
   }
 }
 </script>
