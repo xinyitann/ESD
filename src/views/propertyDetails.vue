37dcebe12165dca7cc6e8c1616e64606b03dd94c
Xinyi Tan <xinyi.tan.2021@scis.smu.edu.sg>
2023-03-31 19:59:34 +0800

    done displaying details

--- src/views/propertyDetails.vue
+++ src/views/propertyDetails.vue
@@ -33,73 +33,13 @@
 
         <div class="p-5 row w-100">
             <div class="col-12 col-md-7 p-2">
-                <h3 class="mb-3">Property Name</h3>
-                <p class="fs-5">$47294423</p>
+                <h3 class="mb-3">{{ property_result.name }}</h3>
+                <p class="fs-5">${{ property_result.estimated_cost }}</p>
                 <hr>
-                <table v-for="p_result in property_result" :key="p_result" class="table table-borderless">
-                    <tr>
-                        <th>
-                            Address
-                        </th>
-                        <td>
-                            {{p_result.address}}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            Postal Code
-                        </th>
-                        <td>
-                            {{p_result.postalcode}}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            Neighbourhood
-                        </th>
-                        <td>
-                            {{p_result.neighbourhood}}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            Property Type
-                        </th>
-                        <td>
-                            {{p_result.property_type}}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            Square Feet
-                        </th>
-                        <td>
-                            {{p_result.square_feet}}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            Number of Rooms
-                        </th>
-                        <td>
-                            {{p_result.room}}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            House Facing Direction
-                        </th>
-                        <td>
-                            {{p_result.facing}}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            Build Year
-                        </th>
-                        <td>
-                            {{p_result.build_year}}
-                        </td>
+                <table class="table table-borderless">
+                    <tr v-for="(value, key) in filtered_result" :key="key" >
+                    <th>{{ key }}</th>
+                    <td>{{ value }}</td>
                     </tr>
                 </table>
             </div>
@@ -108,7 +48,7 @@
                     <h5 class="card-title text-center my-2">Interested?</h5>
                     <div class="card-body">
                         <p>
-                            Contact Jonah @ +65 12345678 / jonah@gmail.com
+                            Contact {{ agent_result.name }} @ +65 {{ agent_result.phone }} / {{ agent_result.email }}
                         </p>
                         <div class="row">
                             <div class="col-4">
@@ -132,7 +72,7 @@
                     <div class="row mb-3">
                         <div class="col">
                             <p class="text-center fw-bold">
-                                $13771
+                                ${{auction_result.starting_price}}
                             </p>
                             <p class="d-flex justify-content-center">
                                 <small class="text-body-secondary">Minimum Bid</small>
@@ -148,7 +88,7 @@
                         </div>
                         <div class="col">
                             <p class="text-center fw-bold">
-                                $13771
+                                ${{auction_result.highest_bid}}
                             </p>
                             <p class="d-flex justify-content-center">
                                 <small class="text-body-secondary">Highest Bid</small>
@@ -156,92 +96,19 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Enter your bid" aria-label="Enter your bid" aria-describedby="button-addon1">
-                        <button class="btn" type="button" style="background-color: #447098; color: white;" id="button-addon">Place Bid</button>
+                        <input v-model='bidAmount' type="text" class="form-control" placeholder="Enter your bid" aria-label="Enter your bid" aria-describedby="button-addon1">
+                        <button class="btn" type="button" style="background-color: #447098; color: white;" id="button-addon"  @click="submitBid()">Place Bid</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-        <!-- <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-xxl-8">
-                            <div class="card border-light">
-                                <div class="card-body" style="background: var(--bs-white);border-style: none;">
-                                    <h4 class="text-center card-title">$600,000</h4>
-                                </div>
-                            </div>
-                            <p>Squarefeet : 1216 sqft</p>
-                            <p>Room : 3 bedrooms, 2 toilets</p>
-                            <p>Address :&nbsp;Blk 345<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Halloween Avenue 6<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Singapore 672044</p>
-                            <p>Build Year : 1995</p>
-                        </div>
-                        <div class="col"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="card border-light"></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col"></div>
-                        <div class="col-xxl-8">
-                            <div class="card border-light">
-
-                                <div class="card-body" style="background: var(--bs-gray-100);">
-                                    <h4 class="text-center card-title">Jonah</h4>
-                                    <div class="row">
-                                        <div class="col d-xxl-flex justify-content-xxl-center align-items-xxl-center" style="text-align: center;">
-                                            <p class="fs-5">Contact Information&nbsp;</p>
-                                        </div>
-                                        <div class="col">
-                                            <p style="text-align: center;">+65 12345678&nbsp;</p>
-                                            <p style="text-align: center;">hello@gmail.com&nbsp;</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col"></div>
-                                        <div class="col-xxl-6 d-xxl-flex justify-content-xxl-center align-items-xxl-center">
-                                            <router-link to="/bookingnow">
-                                            <a class="btn btn-danger float-start" type="button">Book Appointment</a>
-                                        </router-link></div>
-                                            
-                                        <div class="col"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col"></div>
-                        <div class="col-xxl-8">
-                            <div class="card border-light">
-
-                                <div class="card-body">
-                                    <h4 class="text-center card-title">Bidding Details&nbsp;</h4>
-                                    <p class="d-xxl-flex justify-content-xxl-center card-text">5 days 3 hrs / Sunday 10 pm / 7 bids / $ 900,000</p>
-                                    <div class="row">
-                                        <div class="col"></div>
-                                        <div class="input-group"><input class="form-control" type="text" placeholder="Enter your bid" fdprocessedid="4gn2t1a"><button class="btn " type="button" fdprocessedid="82x11p" style="background-color: #447098; color: white;" >Place Bid</button></div>
-                                        <div class="col"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
 </template>
 
 <script>
+// import PropertyCard from '@/components/propertyCard.vue';
+
 //to edit later 
 const get_all_URL = "http://localhost:5009/get_property_details";
 
@@ -257,45 +124,176 @@ name: 'PropertyDetailsPage',
             agent_result:[],
             auction_result:[],
             message: "",
-            property_id: "1",
-            customer_id:"1" //should not be search
+            property_id: "",//passed in from properties
+            customer_id:"", //should not be search
+            filtered_result:[],
+            biddingStartTime:"",
+            biddingEndTime:"",
+
+            customerId:1,
+            // propertyId:1,
+            bidAmount:0,
             
 
         }
     },
+//     props: {
+//     property_id: Number
+//   },
+
     created() {
                 // on Vue instance created, load the book list
                 this.findpropertydetails();
+                this.findagentdetails();
+                
             },
+    computed: {
+        date_time_start(){
+            let final = this.biddingDate
+            let timing = this.biddingStartTime.slice(0,2)
+            timing = timing + ':' + '00'+':'+'00'
+            final = final + ' ' + timing
+            return final
+        },
+        date_time_end(){
+            let final = this.biddingDate
+            let timing = this.biddingEndTime.slice(0,2)
+            timing = timing + ':' + '00'+':'+'00'
+            final = final + ' ' + timing
+            return final
+        }
+    },
+
     methods:{
         //get property details based on complex microservice
-    findpropertydetails() {
-    console.log("here")
-    var search_url = get_all_URL + '/'+ this.property_id +'/'+this.customer_id
-    console.log(search_url)
-    const response = fetch(search_url)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(response);
-        
-        if (data.code === 404) {
-        // no book in db
-        this.message = data.message;
-        } else {
-        this.property_list = data.data.property_result.data;
-        }
-        console.log(this.property_list)
-    })
-    .catch((error) => {
-        // Errors when calling the service; such as network error,
-        // service offline, etc
-        console.log(this.message + error);
-    });
-    },
+        findpropertydetails() {
+        console.log("here")
+        var search_url = get_all_URL + '/'+ this.property_id +'/'+this.customer_id
+        console.log(search_url)
+        const response = fetch(search_url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(response);
+            
+            if (data.code === 404) {
+            // no book in db
+            this.message = data.message;
+            } else {
+            this.property_result = data.data.property_result.data;
+            console.log(this.property_result)
 
+        
+            this.transformObject(this.property_result)
+            }
+        })
+        .catch((error) => {
+            // Errors when calling the service; such as network error,
+            // service offline, etc
+            console.log(this.message + error);
+        });
+        },
+
+        transformObject(input) {
+            const excludedKeys = ['agent_id', 'auction_id','image','property_id','name'];
+        const filtered_result = {};
+        
+        Object.entries(input).forEach(([key, value]) => {
+            if (!excludedKeys.includes(key)) {
+            const capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
+            filtered_result[capitalizedKey] = value;
+            }
+        })
+
+        this.filtered_result=filtered_result
+        console.log(filtered_result)
+
+        // }
+        },
+
+        findagentdetails() {
+        console.log("here is agent")
+        var search_url = get_all_URL + '/'+ this.property_id +'/'+this.customer_id
+        console.log(search_url)
+        const response = fetch(search_url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(response);
+            
+            if (data.code === 404) {
+            // no book in db
+            this.message = data.message;
+            } else {
+            this.agent_result = data.data.agent_result.data;
+            console.log(this.agent_result)
+            }
+        })
+        .catch((error) => {
+            // Errors when calling the service; such as network error,
+            // service offline, etc
+            console.log(this.message + error);
+        });
+        },
+
+        findauctiondetails() {
+        console.log("here is auction")
+        var search_url = get_all_URL + '/'+ this.property_id +'/'+this.customer_id
+        console.log(search_url)
+        const response = fetch(search_url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(response);
+            
+            if (data.code === 404) {
+            // no book in db
+            this.message = data.message;
+            } else {
+            this.auction_result = data.data.auction_result.data;
+            console.log(this.auction_result)
+            }
+        })
+        .catch((error) => {
+            // Errors when calling the service; such as network error,
+            // service offline, etc
+            console.log(this.message + error);
+        });
+        },
+        //idk if it works :o
+        async submitBid(){
+            // let dateTimeStart = this.date_time_end
+            // let dateTimeEnd = this.date_time_end
             
 
-    },
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+
+            "customer_id": this.customerId,
+            "bid_amount": parseInt(this.bidAmount),
+            "property_id":this.propertyId
+        
+            });
+            console.log(raw)
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+            };
+
+            const data_fetch = await fetch("http://localhost:5900/make_bid", requestOptions)
+            this.check = await data_fetch.json()
+            console.log(data_fetch)
+            if (data_fetch['status'] == 201){
+                alert('bidding has been created')
+            } 
+            else{
+                alert('bidding failed')
+            }
+                    },
+
+
+}
 };
 
 </script>
