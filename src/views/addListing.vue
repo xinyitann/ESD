2eea9be23be4a6b1d8d92f7ed50441dfd99b8529
Abhisit7345 <111484215+Abhisit7345@users.noreply.github.com>
2023-04-01 00:53:56 +0800

    Update addListing.vue

--- src/views/addListing.vue
+++ src/views/addListing.vue
@@ -6,31 +6,47 @@
                     <div class="row">
                         <div class="col-12 col-md-6 px-5">
                             <div class="mb-3">
+                                <label for="address" class="form-label">Customer ID*</label>
+                                <input v-model="customer_id" type="text" class="form-control" id="address">
+                            </div>
+                            <div class="mb-3">
                                 <label for="address" class="form-label">Address*</label>
-                                <input type="text" class="form-control" id="address">
+                                <input v-model="address" type="text" class="form-control" id="address">
+                            </div>
+                            <div class="mb-3">
+                                <label for="address" class="form-label">Name*</label>
+                                <input v-model="name" type="text" class="form-control" id="address">
+                            </div>
+                            <div class="mb-3">
+                                <label for="neighbourhood" class="form-label">neighbourhood</label>
+                                <input v-model="neighbourhood" type="text" class="form-control" id="neighbourhood">
                             </div>
                             <div class="mb-3">
                                 <label for="PostalCode" class="form-label">Postal Code*</label>
-                                <input type="text" class="form-control" id="PostalCode">
+                                <input v-model="postalcode" type="text" class="form-control" id="PostalCode">
                             </div>
                             <div class="mb-3">
                                 <label for="buildYear" class="form-label">Build Year*</label>
-                                <input type="text" class="form-control" id="buildYear">
+                                <input v-mode="buildyear" type="text" class="form-control" id="buildYear">
                             </div>
                             <div class="mb-3">
                                 <label for="size" class="form-label">Size in Square Feet*</label>
-                                <input type="text" class="form-control" id="size">
+                                <input v-model="size" type="text" class="form-control" id="size">
                             </div>
                             <div class="mb-3">
                                 <label for="roomNo" class="form-label">Number of Rooms*</label>
-                                <input type="number" class="form-control" id="roomNo">
-                            </div>
-                            <div class="mb-3">
-                                <label for="bathroomNo" class="form-label">Number of Bathrooms*</label>
-                                <input type="number" class="form-control" id="bathroomNo">
+                                <input v-model="rooomNo" type="number" class="form-control" id="roomNo">
                             </div>
                         </div>
                         <div class="col-12 col-md-6 px-5">
+                            <div class="mb-3">
+                                <label for="estimatedCost" class="form-label">Property Type</label>
+                                <input v-model="property_type" type="text" class="form-control" id="estimatedCost">
+                            </div>
+                            <div class="mb-3">
+                                <label for="facing" class="form-label">Facing</label>
+                                <input v-model="facing" type="text" class="form-control" id="facing">
+                            </div>
                             <div class="mb-3">
                                 <label for="estimatedCost" class="form-label">Estimated Cost*</label>
                                 <input type="text" class="form-control" id="estimatedCost">
@@ -48,10 +64,6 @@
                                 <input type="date" class="form-control" id="startBidDate" v-model="startBidDate">
                             </div>
                             <div class="mb-3">
-                                <label for="bidEndDate" class="form-label">Bidding End Date*</label>
-                                <input type="date" class="form-control" id="bidEndDate" v-model="bidEndDate">
-                            </div>
-                            <div class="mb-3">
                                 <label for="images" class="form-label">Images*</label>
                                 <input type="file" id="images" name="images" accept="image/*" class="form-control" multiple>
                             </div>
@@ -61,7 +73,7 @@
                         <router-link to="/myproperty">
                             <button type="submit" class="btn mx-4" style="background-color: #6d8363; color: white;">Cancel</button>
                         </router-link>
-                        <button type="submit" class="btn mx-4" style="background-color: #447098; color: white;">Add Listing</button>
+                        <button type="submit" class="btn mx-4" style="background-color: #447098; color: white;" @click="submit_add_listing()">Add Listing</button>
                     </div>
                 </form>
         </div>
@@ -74,10 +86,75 @@ export default {
 name: 'AddListingPage',
     components: {
     },
+    data(){
+        return {
+            agent_id: this.agent_id_prop,
+            customer_id: '',
+            name: '',
+            address: '',
+            postalcode: '',
+            property_type: '',
+            square_feet: '',
+            room: '',
+            facing: '',
+            build_year: '',
+            estimated_cost: '',
+            image: '',
+            status: '',
+            starting_price: '',
+            option_fee: '',
+            neighbourhood: '',
+            status: 'open'
+        }   
+    },
+    methods: {
+        async submit_add_listing(){
+            
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+            "agent_id" : this.agent_id,
+            "customer_id": this.customer_id,
+            "name": this.name,
+            "address": this.address,
+            "postalcode": this.postalcode,
+            "property_type": this.property_type,
+            "square_feet": this.square_feet,
+            "room": this.room,
+            "facing": this.facing,
+            "build_year": this.build_year,
+            "estimated_cost": this.estimated_cost,
+            "image": this.image,
+            "status": this.status,
+            "starting_price": this.starting_price,
+            "option_fee": this.option_fee,
+            "neighbourhood": this.neighbourhood,
+            "status": 'open'
+
+            });
+            console.log(raw)
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+            };
+
+            const data_fetch = await fetch("http://127.0.0.1:5100/make_booking", requestOptions)
+            if(code==201){
+                alert("listing has been created")
+            }
+        }
+    },
+    props: [
+        'agent_id_prop',
+    ],
 }
 
 </script>
 
 <style>
 
+</style>
 </style>
