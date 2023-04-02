87b900406ce1671d57cc782831072b327a6a6700
chulintian <chulin.tian.2021@scis.smu.edu.sg>
2023-04-02 11:45:21 +0800

    Updates to many pages

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <NavBar :user_type="user_type"></NavBar>
   <router-view @submit_property_data="GetPropertyData($event)" @passdata="GetData($event)" @agent_id="GetAgent($event)" :customer_id_prop='customer_id' :agent_id_prop='agent_id' :user_type="user_type" :property_id_prop="property_id"></router-view>
-    <PropertyCard></PropertyCard>
   <FooterComponent></FooterComponent>
-  
 </template>
 
 <script>
 import NavBar from "./components/navbar.vue"
 import FooterComponent from "./components/footer.vue"
-import PropertyCard from "./components/propertyCard.vue"
 
 export default {
   name: 'App',
   components: {
     NavBar,
     FooterComponent,
-    PropertyCard,
   },
   data(){
     return {
